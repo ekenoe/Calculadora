5ecd483a5553ed75158a8dbbea6388a8e95ff1bc
ekenoe <ekenoe@gmail.com>
2016-01-26 13:25:34 +0100

    jijijijijiji

---
+++ app/src/main/java/com/example/alumno/calculadora/parcial.java
@@ -0,0 +1,13 @@
+package com.example.alumno.calculadora;
+
+/**
+ * Created by alumno on 26/01/2016.
+ */
+public class parcial {
+
+    String nombre;
+    String asignatura;
+    String profesor;
+    String nota;
+
+}
